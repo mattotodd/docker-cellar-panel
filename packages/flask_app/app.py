6cf9849604f779fef2477c2df47198ac61f24766
Matt Sullivan <mattotodd@gmail.com>
2020-10-21 22:14:35 -0400

    add better logging

--- packages/flask_app/app.py
+++ packages/flask_app/app.py
@@ -1,9 +1,12 @@
 import os
 import json
 import atexit
+# import logging
+# import socket
+# from datetime import datetime
+# from logging.handlers import SysLogHandler
 
-from flask import Flask, render_template, jsonify
-from flask_papertrail import PaperTrail
+from flask import Flask, request, render_template, jsonify
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from cellar import default_dt, get_panel_information, read_and_store_cellar_panel, set_control
@@ -15,15 +18,36 @@ DEBUG = os.environ.get('DEBUG', 0) == '1'
 
 app = Flask(__name__)
 
-PAPERTRAIL_HOST = os.environ.get('PAPERTRAIL_HOST', None)
-PAPERTRAIL_PORT = os.environ.get('PAPERTRAIL_PORT', None)
-PAPERTRAIL_APP  = os.environ.get('PAPERTRAIL_APP', str(app))
 
-if PAPERTRAIL_HOST and PAPERTRAIL_PORT:
-    app.config['PAPERTRAIL_HOST'] = PAPERTRAIL_HOST
-    app.config['PAPERTRAIL_PORT'] = PAPERTRAIL_PORT
-    app.config['PAPERTRAIL_APP']  = PAPERTRAIL_APP
-    PaperTrail(app)
+# # LOGGING
+# PAPERTRAIL_HOST = os.environ.get('PAPERTRAIL_HOST', None)
+# PAPERTRAIL_PORT = os.environ.get('PAPERTRAIL_PORT', None)
+# PAPERTRAIL_APP  = os.environ.get('PAPERTRAIL_APP', str(app))
+
+# class ContextFilter(logging.Filter):
+#     hostname = socket.gethostname()
+#     def filter(self, record):
+#         record.hostname = ContextFilter.hostname
+#         return True
+
+# logger = logging.getLogger()
+
+# if not DEBUG and PAPERTRAIL_HOST and PAPERTRAIL_PORT:
+#     syslog = SysLogHandler(address=(PAPERTRAIL_HOST, int(PAPERTRAIL_PORT)))
+#     syslog.addFilter(ContextFilter())
+#     format = f"%(asctime)s %(hostname)s {PAPERTRAIL_APP}: %(message)s"
+#     formatter = logging.Formatter(format, datefmt='%b %d %H:%M:%S')
+#     syslog.setFormatter(formatter)
+#     logger = logging.getLogger()
+#     logger.addHandler(syslog)
+#     logger.setLevel(logging.INFO)
+# # End Logging
+
+# @app.after_request
+# def after_request(response):
+#     timestamp = datetime.now().strftime('%Y-%b-%d %H:%M')
+#     app.logger.info('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#     return response
 
 
 @app.route('/read_cellar_panel')
